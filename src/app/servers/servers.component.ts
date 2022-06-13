9d8946143507665576cd0cc40a26795b298e2431
Tandari <tandari.tamas@gmail.com>
2022-06-13 07:36:21 +0200

    Modified the router

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,15 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onRelodServers() {
+    this.router.navigate(['/servers'], {relativeTo: this.activeRoute});
+    console.log("Active route: "+ this.activeRoute.component?.toString());
   }
 
 }
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user!: {id: number, name: string};
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) { 
+    
+  }
 
   ngOnInit() {
+    this.user = {
+      id: this.activeRoute.snapshot.params['id'],
+      name: this.activeRoute.snapshot.params['name'] 
+    }  
+
+    this.activeRoute.params.subscribe(
+      (params: Params)=>{
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+        console.log("Pina!!!!!"); 
+      }
+    );
   }
+
 
 }
